d5392e3edf03dcefd5921f42cb31a6c5b479fa9f
sunnychen43 <sunnychen2000@gmail.com>
2018-05-24 09:13:06 -0400

    jh

--- background.js
+++ background.js
@@ -1,11 +1,14 @@
 chrome.runtime.onInstalled.addListener(() => {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostEquals: 'students.livingston.org'}
-            })
-            ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
+        chrome.declarativeContent.onPageChanged.addRules([
+            {
+                conditions: [
+                    new chrome.declarativeContent.PageStateMatcher({
+                        pageUrl: {hostEquals: 'students.livingston.org'}
+                    })
+                ],
+                actions: [new chrome.declarativeContent.ShowPageAction()]
+            }
+        ]);
     });
 });
